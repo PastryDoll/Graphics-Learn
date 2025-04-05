239dc1f4c6ea6687f66ee983734a1d70c46e6070
PastryDoll <caio_sky@hotmail.com>
2025-04-05 05:03:22 +0200

    first

---
+++ src/main.cpp
@@ -0,0 +1,112 @@
+#include "shader.hpp"
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <math.h>
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+#define WINDOW_TITLE "Hello World"
+
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+static void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
+    if (!window)
+    {   
+		printf("Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+		printf("Failed to initialize GLAD\n");
+        return -1;
+    }
+    
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
+       -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
+       -0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f // top left 
+   };
+   unsigned int indices[] = {  // note that we start from 0!
+       0, 1, 3,   // first triangle
+       1, 2, 3    // second triangle
+   };    
+    
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO); 
+    glBindVertexArray(VAO);
+    
+    unsigned int VBO;
+    glGenBuffers(1, &VBO); 
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);  
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
+    
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+    glBindVertexArray(0);
+
+    Shader simple_rectangle = createShaderFromFile("shaders/vertex.glsl","shaders/fragment.glsl");
+    
+    glViewport(0, 0, WINDOW_WIDTH , WINDOW_HEIGHT);
+    glClearColor(0.1f, 0.1f, 0.3f, 1.0f);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+        
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // float timeValue = glfwGetTime();
+        // float greenValue = sin(timeValue*10) / 2.0f + 0.5f;
+        // int vertexColorLocation = glGetUniformLocation(shaderProgram, "color");
+        useShader(simple_rectangle);
+        // glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f); // This is set for the current shader program
+
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    deleteShader(simple_rectangle);
+
+    glfwTerminate();
+    return 0;
+}
---
+++ src/shader.hpp
@@ -0,0 +1,151 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h> 
+  
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef _WIN32
+#else
+#include <errno.h>  
+#endif
+
+struct Shader
+{
+    unsigned int ID;
+};
+  
+
+char* readFile(const char* filePath) {
+    FILE* file;
+#ifdef _WIN32
+    errno_t err = fopen_s(&file, filePath, "rb");
+    if (err != 0 || file == NULL) {
+        fprintf(stderr, "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ: %s (Error code: %d)\n", filePath, err);
+        return NULL;
+    }
+#else
+    file = fopen(filePath, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ: %s (Error: %s)\n", filePath, strerror(errno));
+        return NULL;
+    }
+#endif
+
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    if (fileSize < 0 || fileSize > SIZE_MAX - 1) {
+        fclose(file);
+        fprintf(stderr, "ERROR::SHADER::FILE_SIZE_OVERFLOW\n");
+        return NULL;
+    }
+
+    char* buffer = (char*)malloc((size_t)fileSize + 1);
+    if (!buffer) {
+        fclose(file);
+        fprintf(stderr, "ERROR::SHADER::MEMORY_ALLOCATION_FAILED\n");
+        return NULL;
+    }
+
+    size_t bytesRead = fread(buffer, 1, fileSize, file);
+    if (bytesRead != (size_t)fileSize) {
+        fprintf(stderr, "ERROR::SHADER::FILE_READ_FAILED: %s\n", filePath);
+        free(buffer);
+        fclose(file);
+        return NULL;
+    }
+
+    buffer[fileSize] = '\0';
+    fclose(file);
+    return buffer;
+}
+
+void checkCompileErrors(unsigned int shader, const char* type) {
+    int success;
+    char infoLog[1024];
+    if (strcmp(type, "PROGRAM") != 0) {
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+            fprintf(stderr, "ERROR::SHADER_COMPILATION_ERROR of type: %s\n%s\n -- --------------------------------------------------- -- \n", type, infoLog);
+        }
+    } else {
+        glGetProgramiv(shader, GL_LINK_STATUS, &success);
+        if (!success) {
+            glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+            fprintf(stderr, "ERROR::PROGRAM_LINKING_ERROR of type: %s\n%s\n -- --------------------------------------------------- -- \n", type, infoLog);
+        }
+    }
+}
+
+Shader createShaderFromFile(const char* vertexPath, const char* fragmentPath) {
+    Shader shader = {0};
+    char* vertexCode = readFile(vertexPath);
+    char* fragmentCode = readFile(fragmentPath);
+
+    if (!vertexCode || !fragmentCode) {
+        fprintf(stderr, "ERROR::SHADER::FAILED_TO_READ_SHADER_FILES\n");
+        if (vertexCode) free(vertexCode);
+        if (fragmentCode) free(fragmentCode);
+        return shader; 
+    }
+
+    unsigned int vertex, fragment;
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, (const char**)&vertexCode, NULL);
+    glCompileShader(vertex);
+    checkCompileErrors(vertex, "VERTEX");
+
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, (const char**)&fragmentCode, NULL);
+    glCompileShader(fragment);
+    checkCompileErrors(fragment, "FRAGMENT");
+
+    shader.ID = glCreateProgram();
+    glAttachShader(shader.ID, vertex);
+    glAttachShader(shader.ID, fragment);
+    glLinkProgram(shader.ID);
+    checkCompileErrors(shader.ID, "PROGRAM");
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+
+    free(vertexCode);
+    free(fragmentCode);
+
+    return shader;
+}
+
+void useShader(Shader shader) {
+    glUseProgram(shader.ID);
+}
+
+void setBool(Shader shader, const char* name, int value) {
+    glUniform1i(glGetUniformLocation(shader.ID, name), (int)value);
+}
+
+void setInt(Shader shader, const char* name, int value) {
+    glUniform1i(glGetUniformLocation(shader.ID, name), value);
+}
+
+void setFloat(Shader shader, const char* name, float value) {
+    glUniform1f(glGetUniformLocation(shader.ID, name), value);
+}
+
+void setVec3(Shader shader, const char* name, float x, float y, float z) {
+    glUniform3f(glGetUniformLocation(shader.ID, name), x, y, z);
+}
+
+void setMat4(Shader shader, const char* name, const float* mat) {
+    glUniformMatrix4fv(glGetUniformLocation(shader.ID, name), 1, GL_FALSE, mat);
+}
+
+void deleteShader(Shader shader) {
+    glDeleteProgram(shader.ID);
+}
+
+#endif
